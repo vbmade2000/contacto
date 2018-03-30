b7f063931d792ff7efdb9c2a6de8caa2b2313a2d
Malhar Vora <mlvora.2010@gmail.com>
2018-03-31 00:30:30 +0530

    Add configuration management

--- api/main.go
+++ api/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/vbmade2000/contacto/config"
 	"github.com/vbmade2000/contacto/handlers"
+	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
+	var conf config.Config
+	if config.ConfigExists() == false {
+		fmt.Println("contacto.conf not found. Writing default one...")
+		conf = config.GetDefaultConfig()
+		configFile, err := os.OpenFile("contacto.conf", os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0644)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		err = config.WriteConfig(conf, configFile)
+		configFile.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else {
+		fmt.Println("Found contacto.conf. Reading configurations...")
+		configFile, err := os.Open("contacto.conf")
+		conf, err = config.ReadConfig(configFile)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
 	r := gin.Default()
 	r.GET("/contact/:contactID", handlers.GetContact)
 	r.GET("/contacts", handlers.GetContacts)
 	r.POST("/contact", handlers.CreateContact)
 	r.DELETE("/contact/:contactID", handlers.DeleteContact)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(net.JoinHostPort(conf.Host, strconv.Itoa(conf.ServerPort))) // listen and serve on 0.0.0.0:8080
 }
---
+++ config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"github.com/pelletier/go-toml"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+type DBProvider struct {
+	Provider string
+	Port     int
+	Host     string
+}
+type Config struct {
+	ServerPort     int
+	Host           string
+	DBProvider     string
+	DBProviderConf []DBProvider
+}
+
+func WriteConfig(config Config, out io.Writer) error {
+	enc := toml.NewEncoder(out)
+	if err := enc.Encode(config); err != nil {
+		return err
+	}
+	_, err := out.Write([]byte{'\n'})
+	return err
+}
+
+func GetDefaultConfig() Config {
+	conf := Config{}
+	conf.ServerPort = 8080
+	conf.Host = "localhost"
+	conf.DBProvider = "sqlite"
+	return conf
+}
+func ReadConfig(in io.Reader) (Config, error) {
+	config := new(Config)
+	rawData, err := ioutil.ReadAll(in)
+	if err != nil {
+		return *config, err
+	}
+	err = toml.Unmarshal(rawData, config)
+	return *config, err
+}
+
+func ConfigExists() bool {
+	return FileExists("contacto.conf")
+}
+
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
